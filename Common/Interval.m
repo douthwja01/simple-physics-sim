950c3d515e4f76f73a88ac4d12eed9a346ec9c53
James A Douthwaite <douthwaiteja@gmail.com>
2023-11-11 21:03:29 +0000

    Tool tweeks

--- Common/Interval.m
+++ Common/Interval.m
@@ -14,6 +14,49 @@ classdef Interval
                 this.Max = min(2);
             end
         end
+        % Operators
+        function [r] = plus(obj1,obj2)
+            % How addition is done with intervals.
+            assert(isscalar(obj1),"Must be scalar.");
+            assert(isscalar(obj2),"Must be scalar.");
+            
+            if isnumeric(obj1) 
+                obj1 = double(obj1);
+                r = Interval(obj2.Min + obj1,obj2.Max + obj1);
+                return;
+            end
+
+            if isnumeric(obj2)
+                obj2 = double(obj2);
+                r = Interval(obj1.Min + obj2,obj1.Max + obj2);
+                return;
+            end
+
+            if isa(obj1,"Interval") && isa(obj2,"Interval")
+                r = Interval(obj1.Min + obj2.Min,obj1.Max + obj2.Max);
+            end
+        end
+        function [r] = minus(obj1,obj2)
+            % How addition is done with intervals.
+            assert(isscalar(obj1),"Must be scalar.");
+            assert(isscalar(obj2),"Must be scalar.");
+            
+            if isnumeric(obj1) 
+                obj1 = double(obj1);
+                r = Interval(obj2.Min - obj1,obj2.Max - obj1);
+                return;
+            end
+
+            if isnumeric(obj2)
+                obj2 = double(obj2);
+                r = Interval(obj1.Min - obj2,obj1.Max - obj2);
+                return;
+            end
+
+            if isa(obj1,"Interval") && isa(obj2,"Interval")
+                r = Interval(obj1.Min - obj2.Max,obj1.Max - obj2.Min);
+            end
+        end
     end
     methods (Static)
         function [flag] = TestIntersection(a,b)
